d307f95069096e21c1e3c58c537f49b745a42908
feher.levente <feher.levente@28-DIAK16>
2024-10-07 12:39:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1;
+using System.Text;
+using System.Linq;
+
+
+List<Triatlon> Triatlon_list = new List<Triatlon>();
+
+var sr = new StreamReader(@"forras.txt", Encoding.UTF8);
+
+while (!sr.EndOfStream)
+{
+    string[] sor = sr.ReadLine()!.Split(";");
+
+    Triatlon triatlon = new Triatlon();
+
+    triatlon.Nev = sor[0];
+    triatlon.Kor = int.Parse(sor[1]);
+    triatlon.Rajtsz = int.Parse(sor[2]);
+    triatlon.Nem = sor[3];
+    triatlon.Eletkorkateg = sor[4];
+
+
+    Triatlon_list.Add(triatlon);
+
+
+}
+sr.Close();
+
+Console.WriteLine($" {Triatlon_list.Count} versenyző fejezte be a versenyt");
+
+Console.WriteLine("-----------------------------------------");
+double atlagferfikor = Triatlon_list.Where(ferfi => ferfi.Nem == "f").Average(ferfi => 2024 - ferfi.Kor);
+Console.WriteLine($" Átlagférfi kor: {atlagferfikor} ");
+
+
+Console.WriteLine("-----------------------------------------");
+int elit = Triatlon_list.Where(elitek => elitek.Eletkorkateg == "elit junior").Count();
+Console.WriteLine($" {elit} elit junior versenyző van");
+
+Console.WriteLine("-----------------------------------------");
+
+
+
+
+;
---
+++ Triatlon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Triatlon
+    {
+        public string Nev { get; set; }
+        public int Kor { get; set; }
+
+        public int Rajtsz { get; set; }
+
+        public string Nem { get; set; }
+
+        public string Eletkorkateg { get; set; }
+
+        public TimeSpan Uszasido { get; set; }
+        public TimeSpan Elsodepoido { get; set; }
+        public TimeSpan Kerekparido { get; set; }
+        public TimeSpan Masodikdepoido { get; set; }
+        public TimeSpan Futasido { get; set; }
+        public TimeSpan Osszido => Uszasido + Elsodepoido + Kerekparido + Masodikdepoido + Futasido;
+
+        public override string ToString()
+        {
+            return $"\tNév: {Nev} \tSzületési év{Kor} \tRajtszám: {Rajtsz} \tNeme: {Nem} \tKor kategória: {Eletkorkateg} " +
+                $"\tÚszás ideje: {Uszasido} \tElső depóban töltött ideje: {Elsodepoido} \tKerékpározási ideje: {Kerekparido} \tMásodik deóban töltött ideje: {Masodikdepoido} \tFutási ideje: {Futasido} \tÖssz. ideje: {Osszido}";
+        }
+
+
+
+
+
+    }
+}
